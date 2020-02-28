ef7e409cfdf92df41f779f778cde1e086edf5064
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-02-28 12:37:24 +0100

    SDI–session4-Ejercicio-complementario3

--- src/main/java/com/uniovi/repositories/UsersRepository.java
+++ src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -1,10 +1,22 @@
 package com.uniovi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
 
 	User findByDni(String dni);
+	
+
+	@Query("SELECT u FROM User u WHERE (LOWER(u.name) LIKE LOWER(?1) or LOWER(u.lastName) LIKE LOWER(?1))")
+	Page<User> searchByNameAndLastName(Pageable pageable, String searchtext);
+	
+	@Query("Select u from User u where 1=1")
+	Page<User> findAllPaged(Pageable pageable);
+
 }
--- src/main/java/com/uniovi/services/UsersService.java
+++ src/main/java/com/uniovi/services/UsersService.java
@@ -1,14 +1,19 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 import com.uniovi.repositories.UsersRepository;
 
@@ -32,6 +37,10 @@ public class UsersService {
 		return users;
 	} 
 	
+	public Page<User> getUsersPaged(Pageable pageable) {
+		Page<User> users = usersRepository.findAllPaged(pageable);
+		return users;
+	} 
 	public User getUser(Long id) {
 		return usersRepository.findById(id).get();
 	}
@@ -48,4 +57,15 @@ public class UsersService {
 	public void deleteUser(Long id) {
 		usersRepository.deleteById (id);
 	}
+	
+	public Page<User> searchUserByNameAndLastName(Pageable pageable, String searchText, User user) {
+		Page<User> users = new PageImpl<User>(new LinkedList<User>());
+		searchText= "%"+searchText+"%";
+		if (user.getRole().equals("ROLE_ADMIN")) {
+			users = usersRepository.searchByNameAndLastName(pageable, searchText);
+		}
+		
+		return users;
+	}
+
 }
